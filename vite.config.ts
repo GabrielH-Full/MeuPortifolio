94adb6e69887c37df73fdd6a971b41db0930d5f1
GabrielH-Full <gabrielhb2020@gmail.com>
2026-05-05 21:12:00 -0300

    setup deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/MeuPortif-lio/',
+  base: '/MeuPortifolio/',
   plugins: [
     react(),
     tailwindcss(),
